1272dc6be4243982c6fbe7d876e6070362dad2ab
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-01-31 12:04:45 +0100

    give up on XOR

--- xor/main.cpp
+++ xor/main.cpp
@@ -2,41 +2,25 @@
 #include <iostream>
 using namespace std;
 
-void solve(const long *line, long n, long A, long x, long &variations) {
+bool solve(const long *line, long n, long A, long x, long &variations) {
   // base case:
-  // if (n == 1) {
-  //   long B = line[0];
-  //   return (A ^ B) >= x;
-  // }
+  if (n == 1) {
+    long B = line[0];
+    return (A ^ B) >= x;
+  }
 
-  // long B = line[0];
+  long B = line[0];
 
-  if (n == 3) {
+  for (long j = 0; j < n; j++) {
+    long A = line[j];
 
-    bool all = true;
-
-    for (long j = 0; j < n; j++) {
-      long A = line[j];
-      for (long i = j + 1; i < n; i++) {
-
-        long B = line[i];
-        cout << "vals: " << A << " " << B << " " << (A ^ B)
-             << ((A ^ B) >= x ? " TAK" : " NIE") << "\n";
-        if ((A ^ B) >= x) {
-          variations++;
-        } else {
-          all = false;
-        }
-      }
-    }
-    if (all) {
+    cout << "vals: " << A << " " << B << " " << (A ^ B)
+         << ((A ^ B) >= x ? " TAK" : " NIE") << "\n";
+    if (solve(line + j, n - j, A, x ,variations)) {
       variations++;
     }
-  } else {
-    long B = line[0];
-    const long *newLine = line + 1;
-    solve(newLine, n - 1, B, x, variations);
   }
+  return false;
 }
 
 int main() {
