1323617191e444964e1e233a78e5bd972737dd48
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-01-31 13:34:04 +0100

    give up

--- zeg/main.cpp
+++ zeg/main.cpp
@@ -1,6 +1,5 @@
+#include <algorithm>
 #include <iostream>
-#include <unordered_map>
-#include <utility>
 using namespace std;
 
 struct wyspa {
@@ -12,22 +11,6 @@ inline const bool canAcces(const wyspa &A, const wyspa &B) {
   return (A.x < B.x && A.y < B.y) || (A.x > B.x && A.y > B.y);
 }
 
-struct PairHash {
-  template <class T1, class T2>
-  std::size_t operator()(const std::pair<T1, T2>& p) const {
-      auto h1 = std::hash<T1>{}(p.first);
-      auto h2 = std::hash<T2>{}(p.second);
-      return h1 ^ (h2 << 1);
-  }
-};
-
-struct PairEqual {
-  template <class T1, class T2>
-  bool operator()(const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const {
-      return p1.first == p2.first && p1.second == p2.second;
-  }
-};
-
 int main() {
   // ios::sync_with_stdio(false);
   // cin.tie(nullptr);
@@ -37,79 +20,39 @@ int main() {
   cin.ignore();
 
   wyspa islands[n];
+  wyspa originalOrder[n];
 
   for (long i = 0; i < n; i++) {
     long x, y;
     cin >> x >> y;
     cin.ignore();
     islands[i] = {x, y};
+    originalOrder[i] = {x, y};
   }
 
-  unordered_map<std::pair<int, int>, int, PairHash, PairEqual> data;
+  std::sort(islands, islands + (sizeof(islands) / sizeof(islands[0])),
+            [](wyspa a, wyspa b) { return a.x + a.y < b.x + b.y; });
 
-  cout << "PARSED\n";
-
-  // first pass
-  for (long i = 0; i < n; i++) {
-    for (long j = i; j < n; j++) {
-      if (!data.contains(std::make_pair(i, j)) &&
-          canAcces(islands[i], islands[j])) {
-        data[std::make_pair(i, j)] = 1;
-        data[std::make_pair(j, i)] = 1;
-      }
-    }
-  }
-
-  cout << "FIRST PASS COMPLETED\n";
-
-  
-  // connect stuff
-  while (data.size() < n * n - n) {
-    for (const auto &[key, stepsA] : data) {
-      auto [from, via] = key;
-
-      for (const auto &[key, stepsB] : data) {
-        auto [to, via2] = key;
-        if (via != via2)
-          continue;
-        if (from == to)
-          continue;
-
-        // connect!
-        if (!data.contains(std::make_pair(from, to))) {
-          data[std::make_pair(from, to)] = stepsA + stepsB;
-          data[std::make_pair(to, from)] = stepsA + stepsB;
-        } else {
-          int numSteps = data[std::make_pair(from, to)];
-          if (stepsA + stepsB < numSteps) {
-            data[std::make_pair(from, to)] = stepsA + stepsB;
-            data[std::make_pair(to, from)] = stepsA + stepsB;
-          }
-        }
-      }
-    }
-    cout << "NEXT PASS" << data.size() << " OF " << n * n - n << '\n';
-
-  }
-  cout << "END PASS\n";
-
-
-  // print data
-  for (int i = 0; i < n; i++) {
-    int acc = 0;
-
-    for (const auto &[key, value] : data) {
-      auto [a, b] = key;
-      if (a == i) {
-        acc += value;
-      }
-    }
-    cout << acc << "\n";
-  }
-
-  // // dump cache
-  // for (const auto &[key, value] : data) {
-  //   auto [a, b] = key;
-  //   std::cout << a << " -> " << b << ": " << value << std::endl;
+  // for (long i = 0; i < n; i++) {
+  //   cout << islands[i].x << " " << islands[i].y << "\n";
   // }
+
+  for (long i = 0; i < n; i++) {
+    long acc = 0;
+
+    for (long j = 0; j < n; j++) {
+      if (i == j)
+        continue;
+
+      // direct acces
+      if (canAcces(originalOrder[i], originalOrder[j])) {
+        acc++;
+        continue;
+      }
+
+      // find 
+    }
+    cout << "to acces " << i << " " << acc << " moves\n"; 
+  }
+
 }
