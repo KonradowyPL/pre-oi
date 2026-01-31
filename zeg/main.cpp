8dc09b56228556310f4990c9977b98e100658eb3
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-01-31 12:38:42 +0100

    continue ZEG

--- zeg/main.cpp
+++ zeg/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <utility>
 using namespace std;
 
@@ -12,6 +12,22 @@ inline const bool canAcces(const wyspa &A, const wyspa &B) {
   return (A.x < B.x && A.y < B.y) || (A.x > B.x && A.y > B.y);
 }
 
+struct PairHash {
+  template <class T1, class T2>
+  std::size_t operator()(const std::pair<T1, T2>& p) const {
+      auto h1 = std::hash<T1>{}(p.first);
+      auto h2 = std::hash<T2>{}(p.second);
+      return h1 ^ (h2 << 1);
+  }
+};
+
+struct PairEqual {
+  template <class T1, class T2>
+  bool operator()(const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const {
+      return p1.first == p2.first && p1.second == p2.second;
+  }
+};
+
 int main() {
   // ios::sync_with_stdio(false);
   // cin.tie(nullptr);
@@ -20,31 +36,80 @@ int main() {
   cin >> n;
   cin.ignore();
 
-  wyspa wyspy[n];
+  wyspa islands[n];
 
   for (long i = 0; i < n; i++) {
     long x, y;
     cin >> x >> y;
     cin.ignore();
-    wyspy[i] = {x, y};
+    islands[i] = {x, y};
   }
 
-  map<std::pair<int, int>, int> data;
+  unordered_map<std::pair<int, int>, int, PairHash, PairEqual> data;
+
+  cout << "PARSED\n";
 
   // first pass
   for (long i = 0; i < n; i++) {
     for (long j = i; j < n; j++) {
       if (!data.contains(std::make_pair(i, j)) &&
-          canAcces(wyspy[i], wyspy[j])) {
+          canAcces(islands[i], islands[j])) {
         data[std::make_pair(i, j)] = 1;
         data[std::make_pair(j, i)] = 1;
       }
     }
   }
 
-  // dump cache
-  for (const auto &[key, value] : data) {
-    auto [a, b] = key;
-    std::cout << a << " -> " << b << ": " << value << std::endl;
+  cout << "FIRST PASS COMPLETED\n";
+
+  
+  // connect stuff
+  while (data.size() < n * n - n) {
+    for (const auto &[key, stepsA] : data) {
+      auto [from, via] = key;
+
+      for (const auto &[key, stepsB] : data) {
+        auto [to, via2] = key;
+        if (via != via2)
+          continue;
+        if (from == to)
+          continue;
+
+        // connect!
+        if (!data.contains(std::make_pair(from, to))) {
+          data[std::make_pair(from, to)] = stepsA + stepsB;
+          data[std::make_pair(to, from)] = stepsA + stepsB;
+        } else {
+          int numSteps = data[std::make_pair(from, to)];
+          if (stepsA + stepsB < numSteps) {
+            data[std::make_pair(from, to)] = stepsA + stepsB;
+            data[std::make_pair(to, from)] = stepsA + stepsB;
+          }
+        }
+      }
+    }
+    cout << "NEXT PASS" << data.size() << " OF " << n * n - n << '\n';
+
   }
+  cout << "END PASS\n";
+
+
+  // print data
+  for (int i = 0; i < n; i++) {
+    int acc = 0;
+
+    for (const auto &[key, value] : data) {
+      auto [a, b] = key;
+      if (a == i) {
+        acc += value;
+      }
+    }
+    cout << acc << "\n";
+  }
+
+  // // dump cache
+  // for (const auto &[key, value] : data) {
+  //   auto [a, b] = key;
+  //   std::cout << a << " -> " << b << ": " << value << std::endl;
+  // }
 }
