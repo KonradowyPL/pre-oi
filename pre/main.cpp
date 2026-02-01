a4dd3415f45d2e93af00a9a98a79631715d1a476
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-01 13:12:47 +0100

    working PRE

--- pre/main.cpp
+++ pre/main.cpp
@@ -19,68 +19,59 @@ int main() {
   for (long i = 0; i < n; i++) {
     long val;
     cin >> val;
-    // I hate that, but it allows to use set & sort everything automatically (I
-    // am lazy)
-    tablica[i] = (val << 32) + i;
+    // I hate that, but it allows to use set & sort everything automatically
+    // (I am lazy)
+    tablica[i] = val;
   }
 
   for (long questionNum = 0; questionNum < q; questionNum++) {
-    long l; // start
-    long r; // koniec
-    long m; // rozmiar pamięci
-    long k; // index liczby ktora interesuje wincentego
+    long l, r, m, k;
     cin >> l >> r >> m >> k;
     l--;
-    long len = min(r - l + 1, k);
+    r--;
+
+    long len = r - l + 1;
+    long effectiveLen = min(len, k);
     m = min(m, len);
-    // cout << "memSize " << m << "\n";
-    // cout << "len " << len << "\n";
 
-    std::set<long long> memory;
+    std::multiset<long long> memory;
 
-    long index = 0; // index at which we read data from original array
+    long index = 0;
 
-    // read first m bytes from original array
+    // preload memory
     for (; index < m; index++) {
-      memory.insert(tablica[index + l]);
-      // cout << "inserting " << (tablica[index + l] >> 32) << "\n";
+      memory.insert(tablica[l + index]);
+      // cout << "inserting " << tablica[l + index] << "\n";
     }
 
-    long outputed = 0;
-
-    // run computation
-    for (; index <= r - l + 1 && outputed != k - 1; index++) {
-      long long value = *memory.begin();
-      // cout << "got value " << (value >> 32) << "\n";
-      // cout << "index " << index << "\n";
-      memory.erase(memory.begin());
-      memory.insert(tablica[index + l]);
-      outputed++;
-      // cout << "inserting " << (tablica[index + l] >> 32) << "\n\n";
+    // memory larger than subarray
+    if (m == len) {
+      for (int j = 0; j < k - 1; j++) {
+        memory.erase(memory.begin());
+      }
+      cout << (*memory.begin()) << "\n";
+      continue;
     }
 
-    if (outputed == k - 1) {
-      cout << "final value " << (*memory.begin() >> 32) << "\n";
-    } else {
-      cout << "TODO:\n";
-      // for (; outputed != k - 1; index++) {
-      //   long long value = *memory.begin();
-      //   cout << "got value " << (value >> 32) << "\n";
-      //   cout << "index " << index << "\n";
-      //   memory.erase(memory.begin());
-      //   outputed++;
-      //   // cout << "inserting " << (tablica[index + l] >> 32) << "\n";
-      // }
-      // cout << "final value " << (*memory.begin() >> 32) << "\n";
+    long extracted = 0;
+
+    while (index < len && extracted < k - 1) {
+      memory.erase(memory.begin()); // remove smallest
+      memory.insert(tablica[l + index]);
+      index++;
+      extracted++;
     }
-    // cout << "\n\n\n";
 
-    // MA MIEJSCE:
-    // z tablicy to ramu
-
-    // NIE MA MIEJSCA:
-    // dowolna liczba z pamięci -> pierwsze puste miejsce
-    // usunięcie tej liczby z pamięci
-    // return 0;
+    // SUCCES: we successfully found k-th smallest
+    if (extracted == k - 1 && !memory.empty()) {
+      cout << ((*memory.begin())) << "\n";
+    }
+    // NO SUCCES: subarray too short
+    else {
+      long remaining = k - extracted - 1;
+      for (int j = 0; j < remaining; j++)
+        memory.erase(memory.begin());
+      cout << ((*memory.begin())) << "\n";
+    }
   }
 }
