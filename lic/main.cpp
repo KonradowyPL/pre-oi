d764b0729847807919e185a3a9b2e54d147c5a3f
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-02 13:51:26 +0100

    continue LIC

--- lic/main.cpp
+++ lic/main.cpp
@@ -6,6 +6,7 @@
 // p2 says 2
 // p2 says 3
 
+#include <cassert>
 #include <iostream>
 #include <map>
 #include <set>
@@ -23,7 +24,7 @@ int main() {
     long a, b;
     cin >> a >> b;
     segments[i * 2] = a;
-    segments[i * 2 + 1] = b;
+    segments[i * 2 + 1] = b + 1; // inclusive
     valueRanges[i] = {a, b};
   }
   std::sort(segments.begin(), segments.end());
@@ -37,28 +38,67 @@ int main() {
   cout << "\n";
 
   int numSegments = segments.size();
+
+  
   vector<long long> variations(numSegments + 1);
 
   for (int p = 0; p < n; p++) {
+    cout << "starting bider " << p << "\n";
     auto [bidMin, bidMax] = valueRanges[p];
-    // step 1:
+    // step 1: add on top of previous bidders
+    long long segmentStart = 0;
+    for (int i = 1; i < numSegments; i++) {
+      long long segmentEnd = segments[i];
+      // can bid:
+      if (segmentStart >= bidMin && segmentEnd <= bidMax) {
+        long long previousSegmentStart = 0;
+        for (int j = 1; j <= i; j++) {
+          long long previousSegmentEnd = segments[j];
+
+          if (i == j) {
+            // overlapping segment
+            long long segmentLen = segmentEnd - segmentStart;
+            long long value = (segmentLen - 1) * segmentLen / 2;
+            variations[i] *= max(value, (long long)1);
+            cout << "overlapping segment: " << i << " len " << segmentLen
+                 << " mult: " << value << " got: " << variations[i] << "\n";
+          } else {
+            // calculate
+            variations[i] *= max(variations[j], (long long)1);
+            cout << "multiplying by: " << variations[j] << "\n";
+          }
+
+          previousSegmentStart = previousSegmentEnd;
+        }
+      }
+      segmentStart = segmentEnd;
+    }
 
     // step 2: add 1 for every way someone can reach in their range
-
-    long long segmentStart = 0;
+    // this assumes this person will be the first to bid
+    segmentStart = 0;
     for (int i = 1; i < numSegments; i++) {
       long long segmentEnd = segments[i];
 
       // can bid:
-      if (segmentStart >= bidMin && segmentEnd <= bidMax) {
-        variations[i] += segmentEnd - segmentStart + 1;
+      if (segmentStart >= bidMin && segmentEnd <= bidMax + 1) {
+        variations[i - 1] += segmentEnd - segmentStart;
+        cout << "adding: " << segmentEnd - segmentStart << "\n";
       }
       segmentStart = segmentEnd;
     }
-  }
 
+    cout << "arr: ";
+    for (int x : variations) {
+      std::cout << x << " ";
+    }
+    cout << "\n";
+  }
+  long long acc;
   for (int x : variations) {
     std::cout << x << " ";
+    acc += x;
   }
   cout << "\n";
+  cout << "acc " << acc << "\n";
 }
