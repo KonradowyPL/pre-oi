1b7f25cf03abdd7b55791bf1c5e1e1a8d58ca32a
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-04 10:02:13 +0100

    skr: almost working

--- skr/main.cpp
+++ skr/main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <set>
 #include <tuple>
 #include <vector>
 using namespace std;
@@ -13,22 +14,43 @@ int main() {
   long n;
   cin >> n;
 
-  vector<Box> boxes(n + 1);
-
-  long maxSupport = 0;
-  long maxSupportIndex = 0;
+  vector<Box> boxes(n);
 
   for (int i = 0; i < n; i++) {
     long m, w;
     cin >> w >> m;
     boxes[i] = {m, w};
+  }
 
-    if (w > maxSupport) {
-      maxSupport = w;
-      maxSupportIndex = i;
+  std::sort(boxes.begin(), boxes.end(),
+            [](auto a, auto b) -> bool { return a.support < b.support; });
+
+  for (int i = 0; i < n; i++) {
+    cout << boxes[i].support << " " << boxes[i].mass << "\n";
+  }
+
+  long currMass = 0;
+
+  vector<Box> stack;
+
+  for (int i = 0; i < n; i++) {
+    long mass = boxes[i].mass;
+    long support = boxes[i].support;
+
+    currMass += mass;
+    stack.push_back(boxes[i]);
+    cout << "adding " << i << "\n";
+    while (currMass > support) {
+      int idx = 0;
+      for (int j = 1; j < stack.size(); j++) {
+        if (stack[j].mass > stack[idx].mass)
+          idx = j;
+      }
+      currMass -= stack[idx].mass;
+      stack.erase(stack.begin() + idx);
+      cout << "erasing " << idx << "\n";
     }
   }
 
-  cout << "first box: " << maxSupportIndex + 1 << " with max support "
-       << maxSupport << "\n";
+  cout << "STACK: " << stack.size() << "\n";
 }
