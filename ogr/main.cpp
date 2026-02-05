e4ce89390d572e8cc916a5fb4a8524f04e57ac4a
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-05 10:51:10 +0100

    ogr: 50%

--- ogr/main.cpp
+++ ogr/main.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 int main() {
@@ -12,17 +13,54 @@ int main() {
 
   assert(sequence.size() == days);
 
-
-  long coldDays = 0, hotDays = 0;
+  long remainingColdDays = 0, remainingHotDays = 0;
 
   for (auto day : sequence) {
     if (day == 'Z') {
-      coldDays++;
+      remainingColdDays++;
     } else {
-      hotDays++;
+      remainingHotDays++;
     }
   }
 
-  vector<long> treeState(trees, 0);
+  set<long> treeState;
+  long coins = 0;
+
+  for (int i = 0; i < days; i++) {
+    // cout << "day " << i << " " << sequence[i] << "\n";
+
+    if(sequence[i] == 'Z') {
+      // dzien zimny mozna zbierac
+      if (treeState.size() > 0) {
+        auto largest = *treeState.rbegin();
+        // cout << "collecting " << largest << "\n";
+        coins += largest;
+        treeState.erase(largest);
+      } else {
+        // cout << "doing nothing\n";
+      }
+      remainingColdDays--; 
+    } else {
+      // dzien cieply mozna podlewac
+      if (remainingColdDays >= remainingHotDays) {
+        // find largest tree
+        int j = trees;
+        for (; j > 0; j--) {
+          if (!treeState.contains(j)) {
+            break;
+          }
+        }
+        treeState.insert(j);
+        // cout << "can water " << j << "\n";
+      } else {
+        // cout << "doing nothing\n";
+
+      }
+      remainingHotDays--; 
+
+    }
+
+  }
+  cout << coins << " -1\n";
 
 }
