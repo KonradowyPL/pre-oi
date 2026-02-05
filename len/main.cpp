34263eca20f9e35a0a1ee089eb8fb30a833c8bb5
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-05 10:24:59 +0100

    give up on LEN

--- len/main.cpp
+++ len/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <climits>
 #include <iostream>
@@ -8,13 +9,13 @@ int main() {
   long n;
   cin >> n;
 
-  vector<long> values(n);
+  // vector<long> values(n + 1);
   vector<long> indexes(n + 1);
 
   for (int i = 0; i < n; i++) {
     long a;
     cin >> a;
-    values[i] = a;
+    // values[i] = a;
     indexes[a] = i;
   }
 
@@ -22,29 +23,48 @@ int main() {
   // instnieje MAXSYMALNIE JEDNA
   // liczba mniejsza o większym indexie
 
+  for (int i = 0; i < n + 1; i++) {
+    cout << i <<" has index " << indexes[i] << "\n";
+  }
+  cout << "\n";
+
   for (long value = 1; value < n + 1; value++) {
     long selfIndex = indexes[value];
-    long incorrectCount = 0;
 
-    long largestIndex = 0;
-    long secondLargestIndex = 0;
+    vector<long> filteredIndexes;
 
     for (long j = 1; j < value; j++) {
-      long secondLargestIndexIndex = indexes[j];
-      if (secondLargestIndexIndex > selfIndex) {
-        incorrectCount++;
-
-        if (j > largestIndex) {
-          secondLargestIndex = largestIndex;
-          largestIndex = j;
-        } else if (j > secondLargestIndex && j != largestIndex) {
-          secondLargestIndex = j;
-        }
+      long secondIndex = indexes[j];
+      if (secondIndex > selfIndex) {
+        filteredIndexes.push_back(secondIndex);
       }
     }
 
-    if (incorrectCount > 1) {
-      cout << "has to fix " << value << " second largest index " << secondLargestIndex << "\n";
+    if (filteredIndexes.size() > 1) {
+      std::sort(filteredIndexes.begin(), filteredIndexes.end());
+
+      long secondLargest = filteredIndexes[filteredIndexes.size() - 2];
+
+      cout << "computed value " << value << "\n";
+      cout << "second larges index " << secondLargest << "\n";
+      cout << "moving from " << selfIndex << " to " << secondLargest << "\n";
+
+      cout << "diff: " << secondLargest - selfIndex << "\n";
+      // accually move element:
+
+      for (long j = 1; j < n + 1; j++) {
+        long secondIndex = indexes[j];
+        if (secondIndex >= selfIndex && secondIndex <= secondLargest) {
+          indexes[j]--;
+        }
+        indexes[value] = secondLargest;
+      }
+
+
+      for (int i = 0; i < n + 1; i++) {
+        cout << i <<" has index " << indexes[i] << "\n";
+      }
+      cout << "\n";
     }
   }
 }
