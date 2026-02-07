0a25a8404a0f8d50381cd07e60f2c0060afef665
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-07 11:17:07 +0100

    begin route caching

--- sbm/main.cpp
+++ sbm/main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <tuple>
 #include <vector>
 using namespace std;
 
@@ -9,9 +10,29 @@ int main() {
   // n = rows (Y), m = columns (X)
 
   vector<unsigned char> board(n * m);
+  vector<bool> isSpecial(n * m, false);
+  vector<std::tuple<int, int, int>> routes(n * m, {0, 0, 0});
 
   const auto toIndex = [&](int x, int y) -> int { return y * m + x; };
-  const auto moveShip = [&](int &shipX, int &shipY) -> void { 
+  const auto shapeToBit = [&](const char &shape) -> int {
+    switch (shape) {
+    case '^':
+      return 1;
+      break;
+    case 'v':
+      return 2;
+      break;
+    case '>':
+      return 4;
+      break;
+    case '<':
+      return 8;
+      break;
+    default:
+      assert(false);
+    }
+  };
+  const auto moveShip = [&](int &shipX, int &shipY) -> void {
     char vec = board[toIndex(shipX, shipY)];
     // cout << shipX << " " << shipY << " " << vec << "\n";
     switch (vec) {
@@ -30,19 +51,22 @@ int main() {
     default:
       assert(false);
     }
-   };
+  };
 
   // read board
   for (int y = 0; y < n; y++) {
     for (int x = 0; x < m; x++) {
-      cin >> board[toIndex(x, y)];
+      unsigned char c;
+      cin >> c;
+      board[toIndex(x, y)] = c;
     }
   }
 
   long q;
   cin >> q;
+  std::vector<std::tuple<int, int, int, int, char>> questions(q);
 
-  while (q--) {
+  for (int i = 0; i < q; i++) {
     int shipX, shipY, anomalyX, anomalyY;
     char chr;
     cin >> shipY >> shipX >> anomalyY >> anomalyX >> chr;
@@ -52,6 +76,13 @@ int main() {
     shipY--;
     anomalyX--;
     anomalyY--;
+    questions[i] = {shipX, shipY, anomalyX, anomalyY, chr};
+    isSpecial[toIndex(anomalyX, anomalyY)] = true;
+    isSpecial[toIndex(shipX, shipY)] = true;
+  }
+
+  for (int i = 0; i < q; i++) {
+    auto [shipX, shipY, anomalyX, anomalyY, chr] = questions[i];
 
     unsigned char orig = board[toIndex(anomalyX, anomalyY)];
     board[toIndex(anomalyX, anomalyY)] = chr;
@@ -61,18 +92,47 @@ int main() {
 
     long moves = 0;
 
+    // route
+    int currRouteStartX = shipX;
+    int currRouteStartY = shipY;
+    int currRouteLen = 0;
+
     while (true) {
-      moves++;
-      moveShip(shipX, shipY);
+
+      // route load
+      auto [endX, endY, len] = routes[toIndex(shipX, shipY)];
+      if (len != 0) {
+          moves += len;
+          shipX = endX;
+          shipY = endY;
+          // reset current route
+          currRouteStartX = shipX;
+          currRouteStartY = shipY;
+          currRouteLen = 0;
+      } else {
+        moveShip(shipX, shipY);
+        moves++;
+        currRouteLen++;
+      }
+
 
       if (shipX < 0 || shipY < 0 || shipX >= m || shipY >= n) {
         break;
-      } 
-      if (visited[toIndex(shipX, shipY)])  {
+      }
+      if (visited[toIndex(shipX, shipY)]) {
         moves = 0;
         break;
       }
 
+      if ((currRouteLen > 32 || isSpecial[toIndex(shipX, shipY)]) && len == 0) {
+        // cache current route
+        routes[toIndex(currRouteStartX, currRouteStartY)] = {shipX, shipY, currRouteLen};
+        currRouteStartX = shipX;
+        currRouteStartY = shipY;
+        currRouteLen = 0;
+      }
+
+  
       visited[toIndex(shipX, shipY)] = true;
     }
 
