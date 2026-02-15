017491e2410c1f989d007e6dce8123fa104b56a3
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-02-15 20:29:11 +0100

    working DRZ sol

--- drz2/main.cpp
+++ drz2/main.cpp
@@ -5,12 +5,29 @@
 #include <vector>
 using namespace std;
 
+// tree const: (2 ^ (n + 1) - 1) * n => n * 2 ^ n
+// single quest: avg n, max 2^n
+
 auto getParent = [](long index) -> long { return (index - 1) / 2; };
 auto getLeft = [](long index) -> long { return 2 * index + 1; };
 auto getRight = [](long index) -> long { return 2 * index + 2; };
 auto getLeaf = [](long index, int n) -> long { return pow(2, n) - 1 + index; };
 
-bool solve() {}
+bool solve(long indexA, long indexB, const vector<vector<long>> &currTree,
+           const vector<vector<long>> &preTree) {
+
+  if (indexA >= currTree.size() || indexB >= preTree.size())
+    return true;
+
+  auto a = currTree[indexA];
+  auto b = preTree[indexB];
+  if (a != b) {
+    return false;
+  }
+
+  return solve(getLeft(indexA), getLeft(indexB), currTree, preTree) ||
+         solve(getLeft(indexA), getRight(indexB), currTree, preTree);
+}
 
 int main() {
   int n;
@@ -33,25 +50,60 @@ int main() {
   for (long i = 0; i < currNaz.size(); i++) {
     unsigned char c = currNaz[i];
     long index = getLeaf(i, n);
-    cout << "leaf index " << index << ":\n";
-    currTree[index][c - 97]++;
-    for (int j = 0; j < n; j++) {
-      index = getParent(index);
+    for (int j = 0; j <= n; j++) {
       currTree[index][c - 97]++;
-      cout << "index " << index << "\n";
+      index = getParent(index);
     }
   }
-  cout << "second:\n";
   // construct second
   for (long i = 0; i < preNaz.size(); i++) {
     unsigned char c = preNaz[i];
     long index = getLeaf(i, n);
-    cout << "leaf index " << index << ":\n";
-    preTree[index][c - 97]++;
-    for (int j = 0; j < n; j++) {
-      index = getParent(index);
+    for (int j = 0; j <= n; j++) {
       preTree[index][c - 97]++;
-      cout << "index " << index << "\n";
+      index = getParent(index);
+    }
+  }
+
+  auto r = solve(0, 0, currTree, preTree);
+  if (r) {
+    cout << "TAK\n";
+  } else {
+    cout << "NIE\n";
+  }
+
+  while (q--) {
+    long t, k;
+    cin >> t >> k;
+    k--; // to 0 indexed
+    char c;
+    cin >> c;
+    if (t == 1) {
+      auto index = getLeaf(k, n);
+      auto origChar = currNaz[k];
+      currNaz[k] = c;
+
+      for (int j = 0; j <= n; j++) {
+        currTree[index][c - 97]++;
+        currTree[index][origChar - 97]--;
+        index = getParent(index);
+      }
+    } else {
+      auto index = getLeaf(k, n);
+      auto origChar = preNaz[k];
+      preNaz[k] = c;
+
+      for (int j = 0; j <= n; j++) {
+        preTree[index][c - 97]++;
+        preTree[index][origChar - 97]--;
+        index = getParent(index);
+      }
+    }
+    auto r = solve(0, 0, currTree, preTree);
+    if (r) {
+      cout << "TAK\n";
+    } else {
+      cout << "NIE\n";
     }
   }
 }
